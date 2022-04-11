21f8a8d4252205d465346f88b5c60fc8e305b313
CarlosJAVNetto <carlosjavnetto@gmail.com>
2022-04-11 00:54:45 -0300

    Adicionar arquivos de projeto.

---
+++ Desafio1/Bancos.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Desafio1
+{
+    public class Bancos
+    {
+        public int id { get; set; }
+        public string nome { get; set; }
+        public string codigoIspb { get; set; }
+        public string codigo { get; set; }
+        public int status { get; set; }
+        public string convenioCnab240 { get; set; }
+        public string dataCadastro { get; set; }
+        public string dataAlteracao { get; set; }
+
+
+    }
+}
---
+++ Desafio1/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.IO;
+using System.Text.Json;
+using System.Text.Json.Serialization;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Desafio1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Bancos> bancos = new List<Bancos>();
+
+            
+            string arquivoJson = File.ReadAllText(@"JSON_BANCO.txt");
+
+            List<Bancos> banco = JsonSerializer.Deserialize<List<Bancos>>(arquivoJson);
+
+
+            banco = banco.OrderBy(x => x.id).ToList();
+
+            for(int i=0;i< banco.Count; i++)
+            {
+                Console.WriteLine("ID: " + banco[i].id);
+                Console.WriteLine("Nome: " + banco[i].nome);
+                Console.WriteLine("Código ISPB: " + banco[i].codigoIspb);
+                Console.WriteLine("Código: " + banco[i].codigo);
+                Console.WriteLine("Status: " + banco[i].status);
+                Console.WriteLine("Convênio CNAB 240: " + banco[i].convenioCnab240);
+                Console.WriteLine("Data de Cadastro: " + banco[i].dataCadastro);
+                Console.WriteLine("Data de Alteração: " + banco[i].dataAlteracao);
+                Console.WriteLine();
+
+            }
+        }
+    }
+}
